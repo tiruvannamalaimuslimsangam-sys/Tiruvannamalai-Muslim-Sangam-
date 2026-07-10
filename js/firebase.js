870cbbfa63f6f3051c6192b0082bd97e160c9093
tiruvannamalaimuslimsangam-sys <tiruvannamalaimuslimsangam@gmail.com>
2026-07-10 17:53:32 +0530

    Initialize Firebase app and Firestore database

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvAWtYuK6a70wfUUXLK2PKlWS2-SOstCU",
+  authDomain: "tms-membership.firebaseapp.com",
+  projectId: "tms-membership",
+  storageBucket: "tms-membership.firebasestorage.app",
+  messagingSenderId: "91628302504",
+  appId: "1:91628302504:web:7558e2debcf636ccc2529d"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
